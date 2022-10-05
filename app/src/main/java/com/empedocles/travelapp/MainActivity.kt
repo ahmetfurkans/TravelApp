6102453e8306660b376b9337ec1b411b222ab133
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-10-05 23:49:48 +0300

    Add HomeViewModel

--- app/src/main/java/com/empedocles/travelapp/MainActivity.kt
+++ app/src/main/java/com/empedocles/travelapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.empedocles.travelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.empedocles.travelapp.presentation.home.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<HomeViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        observeLiveData()
+    }
+
+    fun observeLiveData() {
+        viewModel.allTravelList.observe(this) {
+
+            this.viewModel.allTravelList.value?.let { it ->
+                it.forEach(){
+                    println(it.category)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/empedocles/travelapp/presentation/home/HomeViewModel.kt
@@ -0,0 +1,62 @@
+package com.empedocles.travelapp.presentation.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.empedocles.travelapp.domain.model.TravelModel
+import com.empedocles.travelapp.domain.repository.AllTravelItemRepository
+import com.empedocles.travelapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: AllTravelItemRepository
+) :ViewModel() {
+    private val _allTravelList = MutableLiveData<List<TravelModel>>()
+    val allTravelList: LiveData<List<TravelModel>> = _allTravelList
+
+    private val _error = MutableLiveData<Boolean>()
+    val error: LiveData<Boolean> = _error
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _hotels = MutableLiveData<List<TravelModel>>()
+    val hotels: LiveData<List<TravelModel>> = _hotels
+
+    private val _flights = MutableLiveData<List<TravelModel>>()
+    val flights: LiveData<List<TravelModel>> = _flights
+
+    private val _transportation = MutableLiveData<List<TravelModel>>()
+    val transportation: LiveData<List<TravelModel>> = _transportation
+
+    init {
+        getAllTravelItem()
+    }
+
+    private fun getAllTravelItem(
+    ) {
+        viewModelScope.launch {
+            when(val result = repository.getAllTravelItem()) {
+                is Resource.Error -> {
+                    _isLoading.value = false
+                    _error.value = true
+                }
+                is Resource.Loading -> {
+                    _isLoading.value = true
+                    _error.value = false
+                }
+                is Resource.Success -> {
+                    _isLoading.value = false
+                    _error.value = false
+                    result.data?.let { data ->
+                        _allTravelList.value = data
+                    }
+                }
+            }
+        }
+    }
+}
