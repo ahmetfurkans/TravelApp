beb78bd66590f64d0dafdbff95fc92ac59a8eaaa
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-10-07 00:22:01 +0300

    Add empty fragment, ssp ssdp

---
+++ app/src/main/java/com/empedocles/travelapp/presentation/guide/GuideFragment.kt
@@ -0,0 +1,19 @@
+package com.empedocles.travelapp.presentation.guide
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.empedocles.travelapp.R
+
+
+class GuideFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guide, container, false)
+    }
+}
