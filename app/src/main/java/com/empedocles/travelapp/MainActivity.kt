ab1f11507ceeaf1321ad3cde0cd59afa039e67ad
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-10-07 11:50:39 +0300

    Add bottom navigation

--- app/src/main/java/com/empedocles/travelapp/MainActivity.kt
+++ app/src/main/java/com/empedocles/travelapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.empedocles.travelapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.empedocles.travelapp.databinding.ActivityMainBinding
 import com.empedocles.travelapp.presentation.detail.DetailViewModel
 import com.empedocles.travelapp.presentation.guide.GuideViewModel
 import com.empedocles.travelapp.presentation.home.HomeViewModel
@@ -11,11 +15,18 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModels<DetailViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            val navHostFragment  = supportFragmentManager
+                .findFragmentById(fragmentContainerView.id) as NavHostFragment
+            NavigationUI.setupWithNavController(binding.bottomNav ,navHostFragment.navController)
+        }
 
         observeLiveData()
     }
